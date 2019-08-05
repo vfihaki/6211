514bb2375e1e0ddd053411b262ab78ffd9894c1f
Viliami Fihaki <30000357@student.toiohomai.ac.nz>
2019-08-05 16:47:14 +1200

    5/08/2019

---
+++ Comp6211/Queue_Stack/Queue_Stack/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Queue_Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue q1 = new Queue();
+            /*
+            while(true)
+            {
+                Console.Write("Please enter a name to add to the queue: ");
+                q1.Enqueue(Console.ReadLine());
+                Console.Write("Would you like add another name (y/n): ");
+                if (Console.ReadLine().ToLower() == "n")
+                {
+                    break;
+                }
+                int counter = q1.Count;
+                for(int i = 0; i < counter; i++)
+                {
+                    Console.WriteLine($"Name: {q1.Dequeue()}");
+                }
+            }*/
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Please enter a name: ");
+                q1.Enqueue(Console.ReadLine());
+            }
+
+            Console.Write("Please enter a name to search for: ");
+            string name = Console.ReadLine();
+
+            if (q1.Contains(name))
+            {
+                int counter = q1.Count;
+                for (int i = 0; i < counter; i++)
+                {
+                    if ((q1.Peek().ToString())!= name)
+                    {
+                        q1.Dequeue();
+                    }
+                    else
+                        break;
+                }
+            }
+            else
+                Console.Write($"{name} was not found in the collection....");
+            foreach (object x in q1)
+            {
+                Console.WriteLine($"{x}\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Comp6211/Queue_Stack/Stack/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+
+namespace Stack_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack s1 = new Stack();
+
+            for(int i = 0; i < 10; i ++)
+            {
+                Console.Write("Please enter a number: ");
+                s1.Push(double.Parse(Console.ReadLine()));
+            }
+
+            Console.Write("Please enter a number to search for: ");
+            double number = double.Parse(Console.ReadLine());
+
+            if(s1.Contains(number))
+            {
+                int counter = s1.Count;
+                for (int i = 0; i < counter; i++)
+                {
+                    if (double.Parse(s1.Peek().ToString()) != number)
+                    {
+                        s1.Pop();
+                    }
+                    else
+                        break;
+                }
+            }
+            else
+                Console.Write($"{number} was not found in the collection....");
+            foreach(object x in s1)
+            {
+                Console.Write(x);
+            }
+        }
+    }
+}
